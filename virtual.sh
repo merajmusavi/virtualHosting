c56588431ef20e18f2a2b9ae0a57f626eba8517f
merajuni7ed <34039418+merajmusavi@users.noreply.github.com>
2023-10-15 11:34:13 +0330

    Update virtual.sh

--- virtual.sh
+++ virtual.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+while true; do
+        read -p "enter your website name" WEBNAME
+        if [ -d "/var/www/html/$WEBNAME" ];then
+                echo "file already exists"
+        else
+sudo mkdir /var/www/html/$WEBNAME
+break
+        fi
+done
+read -p "enter your name " username
+
+htmlcode="<h1>$username</h1>"
+
+echo $htmlcode > /var/www/html/$WEBNAME/index.html
+~                                                         
